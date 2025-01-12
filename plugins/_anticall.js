9fe065a92a0ed93de311ac6deae99d8d5a4976da
dana <danaputra1001@gmail.com>
2025-01-12 15:30:17 +0700

    sumpah harus nya bener

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -3,11 +3,6 @@ let handler = m => m
 handler.before = async function (m) {
   this.ev.on('call', async (call) => {
       if (call[0].status == 'offer') {
-       
-        await this.sessage(call[0].from, {
-          text: "",
-          quoted: call[0]
-        });
         await this.rejectCall(call[0].id, call[0].from);
         await this.updateBlockStatus(call[0].from, "block");
       }
