810bc610ef6630b20bd489a4edfa12440c8e7980
dana <danaputra1001@gmail.com>
2025-01-31 00:22:38 +0700

    penambahan fitur geser

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,27 +1,127 @@
 let fetch = require('node-fetch');
+const { generateWAMessageContent, generateWAMessageFromContent, proto } = require('@adiwajshing/baileys');
+
 let handler = async (m, { usedPrefix, command, conn, args }) => {
   if (!args[0]) throw `*🚩 Example:* ${usedPrefix}${command} Zhao Lusi`;
-  m.reply(wait)
+  m.reply('Please wait...');
+
   try {
-    let response = await fetch(`https://api.betabotz.eu.org/api/search/pinterest?text1=${args[0]}&apikey=${lann}`);
-    let data = await response.json();   
-    let old = new Date()
-    let limit = Math.min(5, data.result.length);
-    for(let i = 1; i < limit; i++) { 
-      await sleep(3000);
-      conn.sendFile(m.chat, data.result[i], 'pin.jpg', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m);
+    const q = encodeURIComponent(args.join(' '));
+    let response = await fetch(`https://api.betabotz.eu.org/api/search/pinterest?text1=${q}&apikey=${lann}`);
+    let data = await response.json();
+    let res = data.result;
+    let nem = await conn.getName(m.sender);
+
+    if (res.length < 1) return m.reply("Error, Foto Tidak Ditemukan");
+
+    let limit = Math.min(10, res.length);
+    let images = res.slice(0, limit);
+    let videos = res.slice(0, limit);
+
+    let push = [];
+    let i = 1;
+
+    async function createImage(url) {
+      const { imageMessage } = await generateWAMessageContent({
+        image: { url }
+      }, {
+        upload: conn.waUploadToServer
+      });
+      return imageMessage;
     }
+
+    for (let pus of images) {
+      push.push({
+        body: proto.Message.InteractiveMessage.Body.fromObject({
+          text: `${pus}`
+        }),
+        footer: proto.Message.InteractiveMessage.Footer.fromObject({
+          text: global.footer
+        }),
+        header: proto.Message.InteractiveMessage.Header.fromObject({
+          title: '',
+          hasMediaAttachment: true,
+          imageMessage: await createImage(pus)
+        }),
+        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+        })
+      });
+    }
+
+    async function createVideo(url) {
+      const { videoMessage } = await generateWAMessageContent({
+        video: { url }
+      }, {
+        upload: conn.waUploadToServer
+      });
+      return videoMessage;
+    }
+
+    for (let pus of videos) {
+      push.push({
+        body: proto.Message.InteractiveMessage.Body.fromObject({
+          text: `${pus}`
+        }),
+        footer: proto.Message.InteractiveMessage.Footer.fromObject({
+          text: global.footer
+        }),
+        header: proto.Message.InteractiveMessage.Header.fromObject({
+          title: '',
+          hasMediaAttachment: true,
+          videoMessage: await createVideo(pus)
+        }),
+        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+          buttons: [
+            {
+              name: "cta_url",
+              buttonParamsJson: `{"display_text":"Lihat Video","cta_type":"1","url":"${pus}"}`
+            }
+          ]
+        })
+      });
+    }
+
+    const msg = generateWAMessageFromContent(m.chat, {
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: `total result: ${limit}`
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: `Hai\nDibawah ini Adalah hasil dari Pencarian Dari:\n${nem}`
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              hasMediaAttachment: false
+            }),
+            carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+              cards: [
+                ...push
+              ]
+            })
+          })
+        }
+      }
+    }, { quoted: m });
+
+    await conn.relayMessage(m.chat, msg.message, {
+      messageId: msg.key.id
+    });
   } catch (e) {
-    throw `${eror}`;
+    throw `Error: ${e.message}`;
   }
-}
+};
 
 handler.help = ['pinterest <keyword>'];
 handler.tags = ['internet', 'downloader'];
-handler.command = /^(pinterest)$/i;
+handler.command = /^(pinterest|pin)$/i;
 
 module.exports = handler;
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
