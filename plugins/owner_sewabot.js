0f695bb47fd1ac0550afb696b3735cb27e543e7c
Offmonpreset <169056210+LikoSopan@users.noreply.github.com>
2025-02-10 11:24:34 +0700

    Update owner_sewabot.js

--- plugins/owner_sewabot.js
+++ plugins/owner_sewabot.js
@@ -1,91 +1,99 @@
-const { proto } = require('@adiwajshing/baileys').default;
-
-function msToDate(ms) {
-    let temp = ms;
-    let days = Math.floor(temp / (24 * 60 * 60 * 1000));
-    let daysms = temp % (24 * 60 * 60 * 1000);
-    let hours = Math.floor((daysms) / (60 * 60 * 1000));
-    let hoursms = daysms % (60 * 60 * 1000);
-    let minutes = Math.floor((hoursms) / (60 * 1000));
-    let minutesms = hoursms % (60 * 1000);
-    let sec = Math.floor((minutesms) / 1000);
-    return `${days} hari ${hours} jam ${minutes} menit`;
-}
-
-let handler = async (m, { conn, args, command, usedPrefix }) => {
-    let who;
-    if (args.length === 1) {
-        who = m.isGroup ? m.chat : args[0];
-    } else if (args.length === 2) {
-        who = args[0];
-    } else {
-        who = m.isGroup ? m.chat : null;
-    }
-
-    switch (command) {
-        case 'addsewa':
-            if (!args[0] || isNaN(args[args.length - 1])) throw `Masukkan angka yang mewakili jumlah hari!\n*Misal : ${usedPrefix + command} <link grup> 30* atau ${usedPrefix + command} 30*`;
-
-            let jumlahHari = 86400000 * args[args.length - 1];
-            let now = new Date() * 1;
-
-            if (!who) throw `Gunakan format yang benar: ${usedPrefix + command} <link grup> <jumlah hari> atau ${usedPrefix + command} <jumlah hari>`;
-            if (!global.db.data.chats[who]) global.db.data.chats[who] = {};
-
-            if (global.db.data.chats[who].expired && now < global.db.data.chats[who].expired) {
-                global.db.data.chats[who].expired += jumlahHari;
-            } else {
-                global.db.data.chats[who].expired = now + jumlahHari;
-            }
-            conn.reply(m.chat, `Berhasil menetapkan hari kadaluarsa untuk Grup ini selama ${args[args.length - 1]} hari.\n\nHitung Mundur: ${msToDate(global.db.data.chats[who].expired - now)}`, m);
-            break;
-
-        case 'delsewa':
-            if (!who) throw `Gunakan format yang benar: ${usedPrefix + command} <link grup>`;
-            if (!global.db.data.chats[who]) throw `Grup tidak ditemukan di database.`;
-
-            global.db.data.chats[who].expired = false;
-            conn.reply(m.chat, `Berhasil menghapus hari kadaluarsa untuk Grup ini`, m);
-            break;
-
-        case 'listsewa':
-            let sewaList = Object.keys(global.db.data.chats).filter(chatId => global.db.data.chats[chatId].expired);
-            if (sewaList.length === 0) {
-                conn.reply(m.chat, `Tidak ada grup yang memiliki masa sewa aktif.`, m);
-            } else {
-                let listText = 'Grup dengan masa sewa aktif:\n\n';
-                for (let chatId of sewaList) {
-                    let remainingTime = global.db.data.chats[chatId].expired - new Date() * 1;
-                    listText += `ID: ${chatId}\nMasa Aktif: ${msToDate(remainingTime)}\n\n`;
+let handler = async (m, { conn, command, usedPrefix, text, participants }) => {
+    const groups = Object.keys(conn.chats)
+    .filter(key => key.endsWith('@g.us'))
+    .map(key => conn.chats[key]);
+    let [id, expired] = text.split('|');
+    if (!text) {
+    const list = groups.map((group, index) => `*${index + 1}.* ${group.subject}`).join('\n');
+    const teks = '`L I S T - G R O U P - J O I N I N G`\n\n'
+    conn.reply(m.chat, `${teks}`+`${list}`, m);
+    } else if (text.length === 1 && /^\d+$/.test(text)) {
+    const index = parseInt(id) - 1;
+    if (index >= 0 && index < groups.length) {
+        let d = new Date(new Date + 3600000)
+        let locale = 'id'
+        let date = d.toLocaleDateString(locale, {
+                 day: 'numeric',
+                 month: 'long',
+                 year: 'numeric' 
+                })
+        let jumlahHari = 86400000 * expired[expired.length - 0];
+        let now = new Date() * 1;
+        let group = groups[index];
+        let who = group.id
+        let namegc = await conn.getName(who);
+        switch(command) {
+            case "addsewa":
+                if (!expired) throw "masukan angka untuk menambah jangka waktu jangka waktu *contoh:* .addsewa <nomor grup>|<hari> "
+                if (!global.db.data.chats[who]) global.db.data.chats[who] = {};
+    
+                if (global.db.data.chats[who].expired && now < global.db.data.chats[who].expired) {
+                    global.db.data.chats[who].expired += jumlahHari;
+                } else {
+                    global.db.data.chats[who].expired = now + jumlahHari;
                 }
-                conn.reply(m.chat, listText, m);
+                let capt = `[ *Groups Notifikasi* ]
+                
+                *Menambahkan jangka waktu sewa group bot.*
+                *Nama group:* ${namegc}
+                *Id group:* ${who}
+                *Tanggal: ${date}
+                *Jangka waktu:* ${jumlahHari}
+                hai all member, terimakasih telah sewa bot kami`
+                await conn.sendMessage(who, { text: capt, 
+   //                   contextInfo: {
+   //                   isForwarded: true, 
+   //                   forwardedNewsletterMessageInfo: {
+   //                   newsletterJid: '120363337047230103@newsletter',
+   //                   newsletterName: `[ *Groups Notifikasi* ]`, 
+   //                   serverMessageId: -1
+   //                   }, 
+   //               }
+            })
+                conn.reply(m.chat, `Berhasil menetapkan hari kadaluarsa untuk Grup ini selama ${expired} hari.\n\nHitung Mundur: ${msToDate(global.db.data.chats[who].expired - now)}`, m);
+                break;
+            case 'delsewa':
+                if (!global.db.data.chats[who]) throw `Grup tidak ditemukan di database.`;
+                global.db.data.chats[who].expired = false;
+                await conn.groupLeave(who)
+                m.reply(`Berhasil menghapus hari kadaluarsa untuk Grup ini, dan keluar dari group ini`);
+                break;
+            case 'setsewa':
+                if (!global.db.data.chats[who]) throw `Grup tidak ditemukan di database.`;
+                if (!expired) throw "masukan angka untuk mengubah jangka waktu *contoh:* .addsewa <nomor grup>|<hari>"
+                global.db.data.chats[who].expired = false;
+                let caption = `[ *Groups Notifikasi* ]
+                
+                *Perubahan jangka waktu sewa group bot.*
+                *Nama group:* ${namegc}
+                *Id group:* ${who}
+                *Tanggal: ${date}
+                *Jangka waktu:* ${jumlahHari}
+                hai all member, owner bot ku telah mengubah waktu sewa gc bot`
+                await conn.sendMessage(who, { text: caption, 
+   //                   contextInfo: {
+   //                   isForwarded: true, 
+   //                   forwardedNewsletterMessageInfo: {
+   //                   newsletterJid: '120363337047230103@newsletter',
+   //                   newsletterName: `[ *Groups Notifikasi* ]`, 
+   //                   serverMessageId: -1
+   //                   }, 
+   //               }
+                });
+                global.db.data.chats[who].expired += jumlahHari;
+                conn.reply(m.chat, `Berhasil menetapkan hari kadaluarsa untuk Grup ini selama ${expired} hari.\n\nHitung Mundur: ${msToDate(global.db.data.chats[who].expired - now)}`, m);
             }
-            break;
-
-        case 'csewa':
-            if (!who) throw `Gunakan format yang benar: ${usedPrefix + command} <link grup>`;
-            if (!global.db.data.chats[who]) throw `Grup tidak ditemukan di database.`;
-
-            let remainingTime = global.db.data.chats[who].expired - new Date() * 1;
-            if (remainingTime > 0) {
-                conn.reply(m.chat, `Masa aktif sewa untuk grup ini adalah ${msToDate(remainingTime)}`, m);
             } else {
-                conn.reply(m.chat, `Grup ini tidak memiliki masa sewa aktif.`, m);
+            conn.reply(m.chat, 'Grup dengan urutan tersebut tidak ditemukan.', m);
             }
-            break;
-
-        default:
-            throw `Perintah tidak valid: ${command}`;
-    }
-}
-
-handler.help = ['addsewa <hari>', 'addsewa <link grup> <hari>', 'delsewa', 'delsewa <link grup>', 'listsewa', 'ceksewa', 'ceksewa <link grup>'];
-handler.tags = ['owner'];
-handler.command = /^(addsewa|delsewa|listsewa|csewa)$/i;
-handler.rowner = /^(addsewa|delsewa|listsewa)$/i;
-handler.group = false;
-handler.private = false;
-
-module.exports = handler;
-
-//By: Offmonpreset
+        } else {
+        conn.reply(m.chat, `â€¢ *Example :* .out nomer`, m);
+        }
+     };
+  handler.help = ['addsewa','dellsewa','setsewa']
+  handler.tags = ['owner']
+  handler.command = /^(addsewa|dellsewa|setsewa)$/i
+     
+  handler.owner = true
+     
+  module.exports = handler
